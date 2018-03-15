09510bed9be7464057849385fbc9369b78a1c792
Bernardo Reis <bernardosouzareis@gmail.com>
2018-03-15 15:43:04 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const DEFAULT_TIMEOUT = 15000; // Milliseconds
+const METHODS = ['count', 'find', 'findOne', 'findOneAndRemove', 'findOneAndUpdate', 'update'];
+
+function addTimeoutToMethod(schema, method, timeout, errorHandler) {
+    schema.pre(method, function () {
+        this.maxTime(timeout);
+    });
+    schema.post(method, function (error, doc, next) {
+        if (errorHandler) {
+            errorHandler(error);
+        }
+        next();
+    });
+}
+
+function validateOptions(timeout, errorHandler) {
+    const errors = [];
+    if (typeof errorHandler !== 'function') {
+        errors.push({
+            field: 'errorHandler',
+            message: 'Parameter provided is not a function'
+        });
+    }
+    let intValue = parseInt(timeout, 10);
+    if (Number.isNaN(intValue) || intValue.toString() !== timeout.toString()) {
+        errors.push({
+            field: 'timeout', 
+            message: 'Invalid number provided'
+        });
+    }
+    if (errors.length) {
+        const error = new Error('Invalid parameters provided');
+        error.details = errors;
+        throw error;
+    }
+}
+
+/**
+ * Plugin to add query timeouts to a schema
+ * @param {Object} options - options to configure the plugin
+ * @param {Number} options.timeout - Query timeout in miliseconds
+ * @param {Function} options.errorHandler - Function to be called with the error thrown by mongo in case the query timesout
+ * @param {Object} options.methods - By default timeouts will be added to `count`, `find`, `findOne`,
+ * `findOneAndRemove`, `findOneAndUpdate` and `update`. If you want to prevent some of those methods from
+ * having timeouts, provide an object with the properties being the name of the method and false as the value
+ */
+function QueryTimeout(options = {}) {
+    const { 
+        timeout = DEFAULT_TIMEOUT,
+        methods = {},
+        errorHandler = () => {}
+    } = options;
+    validateOptions(timeout, errorHandler);
+
+    return (schema) => {
+        METHODS.forEach((method) => {
+            if (methods[method] === false) {
+                return;
+            }
+            addTimeoutToMethod(schema, method, timeout, errorHandler);
+        });
+    };
+}
+
+QueryTimeout.DEFAULT_TIMEOUT = DEFAULT_TIMEOUT;
+QueryTimeout.METHODS = METHODS;
+
+module.exports = QueryTimeout;
---
+++ test/mongoose-query-timeout.spec.js
@@ -0,0 +1,94 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const QueryTimeout = require('../index');
+
+describe('mongoose-query-timeout.spec.js', function () {
+    let sandbox;
+
+    function createMockSchema() {
+        return {
+            timeout: QueryTimeout.DEFAULT_TIMEOUT,
+            maxTime(timeout) {
+                this.timeout = timeout;
+            },
+            pre(method, callback) {
+                callback.call(this);
+            },
+            post(method, callback) {
+                this.postCallback = callback;
+            },
+            postCallback(error, doc, next) {
+                next();
+            }
+        };
+    }
+
+    beforeEach(function () {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('Should throw an error if provided timeout is an invalid number', function () {
+        const schema = createMockSchema();
+        const options = {
+            timeout: 'NaN'
+        };
+        expect(QueryTimeout.bind(QueryTimeout, options))
+            .to.throw(Error)
+            .with.deep.nested.property('details[0].message', 'Invalid number provided');
+    });
+
+    it('Should throw an error if provided errorHandler is an invalid function', function () {
+        const schema = createMockSchema();
+        const options = {
+            errorHandler: 'Not a function'
+        };
+        expect(QueryTimeout.bind(QueryTimeout, options))
+            .to.throw(Error)
+            .with.deep.nested.property('details[0].message', 'Parameter provided is not a function');
+    });
+
+    it('should use the default timeout in case none is provided', function () {
+        const schema = createMockSchema();
+        sinon.spy(schema, 'maxTime');
+        QueryTimeout()(schema);
+        expect(schema.maxTime.firstCall.args[0]).to.be.eql(QueryTimeout.DEFAULT_TIMEOUT);
+    });
+
+    it('should use the provided timeout', function () {
+        const schema = createMockSchema();
+        sinon.spy(schema, 'maxTime');
+        QueryTimeout({ timeout: 5000 })(schema);
+        expect(schema.maxTime.firstCall.args[0]).to.be.eql(5000);
+    });
+
+    it('should set timeout for the default schema methods', function () {
+        const schema = createMockSchema();
+        sinon.spy(schema, 'pre');
+        QueryTimeout()(schema);
+        const expectedMethods = QueryTimeout.METHODS;
+        const calls = schema.pre.getCalls();
+        const actualMethods = calls.map((call) => call.args[0]);
+        expect(actualMethods).to.include.members(expectedMethods);
+    });
+
+    it('shouldn\'t set timeout to method specified in options', function () {
+        const schema = createMockSchema();
+        sinon.spy(schema, 'pre');
+        QueryTimeout({ methods: { find: false } })(schema);
+        const calls = schema.pre.getCalls();
+        const actualMethods = calls.map((call) => call.args[0]);
+        expect(actualMethods).to.not.include('find');
+    });
+
+    it('should invoke the error handler with the error thrown by mongo', function () {
+        const schema = createMockSchema();
+        const errorHandler = sinon.stub();
+        QueryTimeout({ errorHandler })(schema);
+        schema.postCallback(new Error('unitTestError'), null, () => {});
+        sinon.assert.calledOnce(errorHandler);
+    });
+});
